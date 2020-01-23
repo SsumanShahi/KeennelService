2adfbec96003e1585aad6659b6371ad88d36e304
SsumanShahi <shaggysy201118@gmail.com>
2020-01-23 22:13:49 +0545

    LoginBll class is created for taking the feild name username and password only for login access

--- app/src/main/java/com/suman/kennelservice/BLL/LoginBLL.java
+++ app/src/main/java/com/suman/kennelservice/BLL/LoginBLL.java
@@ -1,4 +1,35 @@
 package com.suman.kennelservice.BLL;
 
+import com.suman.kennelservice.Url.url;
+import com.suman.kennelservice.api.Userapi;
+import com.suman.kennelservice.serverresponse.SignupResponse;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Url;
+
 public class LoginBLL {
+    boolean isSuccess = false;
+
+    public boolean checkUser(String username, String password) {
+
+        Userapi userapi = url.getInstance().create(Userapi.class);
+        Call<SignupResponse> usersCall = userapi.checkUser(username, password);
+
+        try {
+            Response<SignupResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful() &&
+                    loginResponse.body().getStatus().equals("Login success!")) {
+
+                url.token += loginResponse.body().getToken();
+                // Url.Cookie = imageResponseResponse.headers().get("Set-Cookie");
+                isSuccess = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
 }
