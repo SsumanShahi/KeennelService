38983022f593df1b27e84625c7c74f8ad264d03e
SsumanShahi <shaggysy201118@gmail.com>
2020-01-24 01:20:29 +0545

    image path and permission is access from the external storage

--- app/src/main/java/com/suman/kennelservice/activity/RegisterActivity.java
+++ app/src/main/java/com/suman/kennelservice/activity/RegisterActivity.java
@@ -1,10 +1,15 @@
 package com.suman.kennelservice.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -132,7 +137,7 @@ public class RegisterActivity extends AppCompatActivity {
     private void saveImageOnly() {
         File file = new File(imagePath);
         RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"),file);
-        MultipartBody.Part body = MultipartBody.Part.createFormData("image",
+        MultipartBody.Part body = MultipartBody.Part.createFormData("myFile",
                 file.getName(),requestBody);
 
         Userapi userapi = url.getInstance().create(Userapi.class);
@@ -163,6 +168,9 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void browseimage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
     }
 
     private void signup() {
@@ -210,5 +218,33 @@ public class RegisterActivity extends AppCompatActivity {
 //
 //
 //        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(data == null)
+            {
+                Toast.makeText(this, "Please select an image", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        Uri uri = data.getData();
+        profileimg.setImageURI(uri);
+        imagePath = getRealPathFromUri(uri);
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(),uri,projection, null
+                ,null,null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
     }
 }
