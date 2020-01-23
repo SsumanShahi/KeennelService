b2083b4432d23203b724242f51c5aa7e790f0c08
SsumanShahi <shaggysy201118@gmail.com>
2020-01-23 21:11:49 +0545

    Sharepreferenced code changed in SplashScreen Activity

--- app/src/main/java/com/suman/kennelservice/SplashActivity.java
+++ app/src/main/java/com/suman/kennelservice/SplashActivity.java
@@ -25,29 +25,29 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                SharedPreferences sharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
-                String user = sharedPreferences.getString("user","");
-                String psw = sharedPreferences.getString("psw","");
-
-                if(user.equals("admin") ||
-                        psw.equals("admin"))
-                {
-                    Toast.makeText(SplashActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(SplashActivity.this,Dashboard.class);
-                    startActivity(intent);
-                    finish();
-                }
-                else
-                {
-                    Toast.makeText(SplashActivity.this, "Either username of password is incorrect", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
+//                SharedPreferences sharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
+//                String user = sharedPreferences.getString("user","");
+//                String psw = sharedPreferences.getString("psw","");
 //
-//                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-//                startActivity(intent);
-//                finish();//close the activity
+//                if(user.equals("admin") ||
+//                        psw.equals("admin"))
+//                {
+//                    Toast.makeText(SplashActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(SplashActivity.this,Dashboard.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//                else
+//                {
+//                    Toast.makeText(SplashActivity.this, "Either username of password is incorrect", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+////
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();//close the activity
             }
         },2000);
     }
---
+++ app/src/main/java/com/suman/kennelservice/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.suman.kennelservice.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
