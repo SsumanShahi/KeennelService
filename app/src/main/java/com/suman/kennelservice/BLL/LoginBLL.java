b684768058fcf6d7310381019d0596108f379a2e
SsumanShahi <shaggysy201118@gmail.com>
2020-01-23 21:54:59 +0545

    url class is created to connect the server of the api

---
+++ app/src/main/java/com/suman/kennelservice/BLL/LoginBLL.java
@@ -0,0 +1,4 @@
+package com.suman.kennelservice.BLL;
+
+public class LoginBLL {
+}
---
+++ app/src/main/java/com/suman/kennelservice/Url/url.java
@@ -0,0 +1,22 @@
+package com.suman.kennelservice.Url;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class url {
+    //    public static final String Base_Url ="http://192.168.1.11:3000/";
+    public static final String Base_Url="http://10.0.2.2:3000/";
+//    public static final String Base_Url="http://172.100.100.5:3000/";
+
+    public static String token="Bearer ";
+    public static String imagePath= Base_Url+"uploads/";
+
+    public static Retrofit getInstance()
+    {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Base_Url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
